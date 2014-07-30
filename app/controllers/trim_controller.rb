0aaef3d001f1f22bd0aceaa6c291dd23fff14fa0
Membership Application Account <member@diamond.vino-rojo.com>
2014-07-30 14:34:54 -0700

    another commit.

--- app/controllers/trim_controller.rb
+++ app/controllers/trim_controller.rb
@@ -4,6 +4,23 @@ class TrimController < ApplicationController
   end
   def new
   end
+  def create
+    trim_params
+    trim = params[:trim]
+    t = Trim.new
+    t.name = trim[:name]
+    t.language = trim[:language]
+    t.value = trim[:value]
+    t.notes = trim[:notes]
+    if t.save!
+      flash[:success] = 'Trim Created'
+      redirect_to :action => :index
+    else
+      flash[:error] = 'Trim Not Created'
+      redirect_to :action => :new
+    end
+
+  end
   private
     def trim_params
       params.require(:trim).permit(:name, :language, :value, :notes )
